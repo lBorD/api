14bcbe2a7e9f669d2242c61084c2e4eeacf97494
Gabriel RIbeiro <gabriel.ribeiro@tecnospeed.com.br>
2025-03-01 11:46:25 -0300

    criadas rotas de clientes

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,10 +1,19 @@
-// routes/clientRoutes.js
 import express from 'express';
+import ClientController from '../controllers/client.js';
+
 const router = express.Router();
 
-router.post('/clients', (req, res) => {
-  // Lógica para cadastrar um cliente
-  res.json({ message: 'Cliente cadastrado com sucesso!' });
-});
+// Rotas de clientes
+router.post('/register', ClientController.registerClient); // Registrar cliente
+
+router.get('/search', ClientController.listClients); // Listar clientes
+router.get('/search/:id', ClientController.getClientById); // Consultar cliente por ID
+
+router.put('/clients/:id', ClientController.updateClient); // Atualizar cliente
+router.delete('/clients/:id', ClientController.deleteClient); // Eliminar cliente 
+
+router.get('/search/by-name', ClientController.listClientsByName); // Listar clientes por nome
+router.get('/search/by-lastname', ClientController.listClientsByLastName);   // Listar clientes por sobrenome
+router.get('/search/by-phone', ClientController.listClientsByPhone); // Listar clientes por telefone
 
 export default router;
