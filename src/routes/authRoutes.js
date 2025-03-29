78cbe792754516b204b56ac2704a6d52dcf57cb4
BorD <gabriel.ribeiro-bol@live.com>
2025-03-29 11:29:54 -0300

    fix routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import login from '../controllers/login.js';
-import userRegister from '../controllers/userRegister.js';
 
 const router = express.Router();
 
 router.post('/login', login);
-router.post('/user', userRegister)
 
 export default router;
