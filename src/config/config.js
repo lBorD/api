8c6a53429fd50807375486b2f2ed8123614f5353
BorD <gabriel.ribeiro-bol@live.com>
2025-03-29 13:07:37 -0300

    new migrations connection for render

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,12 @@ export default {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
   },
   production: {
     username: process.env.DB_USER,
