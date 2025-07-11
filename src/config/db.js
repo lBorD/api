75a6079d4075ee91c022bbcc9d2906ee527bd2a5
BorD <gabriel.ribeiro-bol@live.com>
2025-07-11 17:15:35 -0300

    fix ssl connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,15 +3,27 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const options = {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT || 'postgres',
+};
+
+if (isProduction) {
+  options.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  };
+}
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'postgres',
-  }
+  options
 );
 
 export default sequelize;
